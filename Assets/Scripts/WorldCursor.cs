e23586f59f492612d529f0e35a8b47b9ef5c6c03
Polona Caserman <polona.caserman@gmail.com>
2017-03-21 15:13:01 +0100

    Initial commit

---
+++ Assets/Scripts/WorldCursor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCursor : MonoBehaviour {
+
+	private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+		meshRenderer = this.GetComponent<MeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Do a raycast into the world based on the user's head position and orientation.
+		Vector3 headPosition = Camera.main.transform.position;
+		Vector3 gazeDirection = Camera.main.transform.forward;
+
+		RaycastHit hitInfo;
+
+        Debug.Log(this.transform.position.x + " " + this.transform.position.y + " " + this.transform.position.z);
+
+		if (Physics.Raycast(headPosition, gazeDirection, out hitInfo)) {
+			meshRenderer.enabled = true;
+
+			// Move thecursor to the point where the raycast hit.
+			this.transform.position = hitInfo.point;
+
+			// Rotate the cursor to hug the surface of the hologram.
+			this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+		} else {
+			// If the raycast did not hit a hologram, hide the cursor mesh.
+			meshRenderer.enabled = false;
+		}
+	}
+}
